b4e4af2a332015faabbc1f82651f2e81d167721e
Nguyen Van Tam <tamnvhustcc@gmail.com>
2024-09-04 19:01:27 +0700

    update

--- install.sh
+++ install.sh
@@ -1,50 +1,66 @@
 #!/bin/bash
 
+total_steps=5
+
+log_step() {
+    step=$1
+    step_title=$2
+    echo "[$step/$total_steps] - $step_title"
+}
+
+handle_error() {
+    error_message=$1
+    echo "Error: $error_message"
+    exit 1
+}
+
 check_python_version() {
+    log_step 1 "Checking Python version"
     if command -v python3 &>/dev/null; then
         python_version=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
         if [ "$(printf '%s\n' "3.9" "$python_version" | sort -V | head -n1)" = "3.9" ]; then
-            echo "Python version $python_version is installed and meets the minimum requirement."
+            return 0
         else
-            echo "Error: Python version 3.9 or higher is required. Your version: $python_version"
-            exit 1
+            handle_error "Python version 3.9 or higher is required. Your version: $python_version"
         fi
     else
-        echo "Error: Python 3 is not installed. Please install Python 3.9 or higher."
-        exit 1
+        handle_error "Python 3 is not installed. Please install Python 3.9 or higher."
     fi
 }
 
 create_virtual_environment() {
-    echo "Creating virtual environment..."
-    python3 -m venv "$HOME/.local/screenvivid_env"
-    source "$HOME/.local/screenvivid_env/bin/activate"
+    log_step 2 "Creating virtual environment"
+    python3 -m venv "$HOME/.local/screenvivid_env" || handle_error "Failed to create virtual environment."
+    source "$HOME/.local/screenvivid_env/bin/activate" || handle_error "Failed to activate virtual environment."
 }
 
 install_app() {
-    echo "Installing ScreenVivid..."
-    git clone https://github.com/tamnguyenvan/screenvivid.git $HOME/.local/screenvivid_env/screenvivid
-    cd $HOME/.local/screenvivid_env/screenvivid && pip install -r requirements.txt
+    log_step 3 "Installing ScreenVivid"
+    if [ ! -d $HOME/.local/screenvivid_env/screenvivid ]; then
+        git clone https://github.com/tamnguyenvan/screenvivid.git $HOME/.local/screenvivid_env/screenvivid || handle_error "Failed to clone the repository."
+    fi
+    cd $HOME/.local/screenvivid_env/screenvivid || handle_error "Failed to navigate to the ScreenVivid directory."
+    pip install -r requirements.txt -q -q -q --exists-action i || handle_error "Failed to install required packages."
 }
 
 create_startup_script() {
-    echo "Creating startup script..."
+    log_step 4 "Creating startup script"
     cat > "$HOME/.local/bin/screenvivid" << EOL
 #!/bin/bash
 source "$HOME/.local/screenvivid_env/bin/activate"
 cd $HOME/.local/screenvivid_env/screenvivid
 python -m screenvivid.main
 EOL
-    chmod +x "$HOME/.local/bin/screenvivid"
+    chmod +x "$HOME/.local/bin/screenvivid" || handle_error "Failed to make the startup script executable."
 }
 
 create_desktop_file() {
-    echo "Creating desktop entry..."
+    log_step 5 "Creating desktop entry"
     cat > "$HOME/.local/share/applications/screenvivid.desktop" << EOL
 [Desktop Entry]
 Name=ScreenVivid
-Exec=/home/tamnv/.local/bin/screenvivid
-Icon=/home/tamnv/.local/share/icons/screenvivid.png
+Exec=$HOME/.local/bin/screenvivid
+Icon=$HOME/.local/share/icons/screenvivid.png
 Type=Application
 Categories=Utility;
 Comment=ScreenVivid Application
@@ -52,13 +68,13 @@ EOL
 }
 
 download_icon() {
-    echo "Downloading application icon..."
     icon_url="https://raw.githubusercontent.com/tamnguyenvan/screenvivid/main/screenvivid/resources/icons/screenvivid.png"
     icon_path="$HOME/.local/share/icons/screenvivid.png"
-    mkdir -p "$(dirname "$icon_path")"
-    curl -o "$icon_path" "$icon_url"
+    mkdir -p "$(dirname "$icon_path")" || handle_error "Failed to create directory for the icon."
+    curl -sS -o "$icon_path" "$icon_url" || handle_error "Failed to download the application icon."
 }
 
+# Execute the steps
 check_python_version
 create_virtual_environment
 install_app
@@ -66,4 +82,4 @@ create_startup_script
 create_desktop_file
 download_icon
 
-echo "Installation completed. You can now find and run ScreenVivid from your application menu."
+echo "Installation completed successfully. You can now find and run ScreenVivid from your application menu."
