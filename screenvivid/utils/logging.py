3a40398e2b1b16782a8a2db609c5cb004a44fa1c
Nguyen Van Tam <tamnvhustcc@gmail.com>
2024-09-25 17:35:49 +0700

    update

--- screenvivid/utils/logging.py
+++ screenvivid/utils/logging.py
@@ -8,7 +8,10 @@ logger.remove()
 log_level = os.getenv("SCREENVIVID_LOG_LEVEL", "INFO")
 if log_level not in ["DEBUG", "INFO", "WARNING", "ERROR"]:
     log_level = "INFO"
-logger.add(sys.stderr, level=log_level)
+
+# Check if sys.stderr is not None before adding it as a handler
+if sys.stderr is not None:
+    logger.add(sys.stderr, level=log_level)
 
 logger.add(
     os.path.join(tempfile.gettempdir(), "screenvivid.log"),
